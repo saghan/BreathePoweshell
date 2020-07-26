4e83a7be685059a575aadc49b36060e6613b5933
Saghan Mudbhari <samudbha@microsoft.com>
2020-07-26 11:59:38 -0700

    work in progress 2

--- 1.ps1
+++ 1.ps1
@@ -1,31 +1,39 @@
-﻿$file_data = Get-Content $PSScriptRoot"\quotes.txt"
+﻿[string[]]$file_data = Get-Content $PSScriptRoot"\quotes.txt" -Raw
+$file_data = $file_data.Split([Environment]::NewLine, [StringSplitOptions]::RemoveEmptyEntries)
+Write-Host $file_data.Length
 $line_num =0
+
+foreach($line in $file_data){
+Write-Host $line
+}
+
 while($true){
 $StartTime = Get-Date
     $EndTime = $StartTime.AddSeconds(6)
 $ErrorActionPreference = "Stop"
 
-$notificationTitle = "Notification: " + [DateTime]::Now.ToShortTimeString()
-$notificationContent = "notification content"
+[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
+[Windows.UI.Notifications.ToastNotification, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
+[Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime] | Out-Null
 
-[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
-$template = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastText01)
+$APP_ID = '110366bd-56e2-47ed-9bdf-3ce1fa408b6c'
 
-#Convert to .NET type for XML manipulation
-$toastXml = [xml] $template.GetXml()
-$toastXml.GetElementsByTagName("text").AppendChild($toastXml.CreateTextNode($notificationTitle)) > $null
+$line = $file_data[$line_num%$file_data.Length]
 
-#Convert back to WinRT type
+$template = @"
+<toast>
+    <visual>
+        <binding template="ToastText02">
+            <text id="1">Relax</text>
+            <text id="2">$line</text>
+        </binding>
+    </visual>
+</toast>
+"@
+$line_num++
 $xml = New-Object Windows.Data.Xml.Dom.XmlDocument
-$xml.LoadXml($toastXml.OuterXml)
-
-$toast = [Windows.UI.Notifications.ToastNotification]::new($xml)
-$toast.Tag = "PowerShell"
-$toast.Group = "PowerShell"
-$toast.ExpirationTime = [DateTimeOffset]::Now.AddSeconds(30)
-#$toast.SuppressPopup = $true
-
-$notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier("PowerShell")
-$notifier.Show($toast);
+$xml.LoadXml($template)
+$toast = New-Object Windows.UI.Notifications.ToastNotification $xml
+[Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($APP_ID).Show($toast)
 Start-Sleep -Seconds $( [int]( New-TimeSpan -End $EndTime ).TotalSeconds )
 }
